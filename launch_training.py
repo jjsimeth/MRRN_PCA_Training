ff86bec6e63b650ba7c39dece2c2f66a0dd425c0
r.bosschaert <r.bosschaert@nki.nl>
2024-10-09 09:56:28 +0200

    Add launch_training.py as a launcher file for Windows and put the training code in a function.

---
+++ launch_training.py
@@ -0,0 +1,18 @@
+import multiprocessing as mp
+import PCA_Training_SWI2 as train
+import platform as plat
+
+if __name__ == '__main__':
+    # Check what os we are running on.
+    os_name = plat.system()
+
+    # Enable multiprocessing if on windows
+    if os_name == 'Windows':
+        print(f" Detected os is: {os_name}. Freeze support is required to run the application")
+        mp.freeze_support()
+    else:
+        print(f" Detected os is: {os_name}. Freeze support is not needed to run the application")
+        pass
+
+    # Start training
+    train.train_stuff()
